635267832b8ae6150d48dad6e18986b2f05dc544
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-01-04 18:50:33 +0530

    Aded Go Rest add comment negative scenario test

---
+++ src/main/java/pojo/GoRest.java
@@ -0,0 +1,23 @@
+package pojo;
+
+import java.util.List;
+
+public class GoRest {
+	
+	private String meta;
+	private List<Data> data;
+	public String getMeta() {
+		return meta;
+	}
+	public void setMeta(String meta) {
+		this.meta = meta;
+	}
+	public List<Data> getData() {
+		return data;
+	}
+	public void setData(List<Data> data) {
+		this.data = data;
+	}
+	
+
+}
