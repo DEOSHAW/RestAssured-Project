f954fc27271ed22a398dfe7cc182f731b01f4dac
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-05-27 10:37:19 +0530

    Added basic auth alternative method test

--- src/test/java/testscripts/ToolsQAAPITest.java
+++ src/test/java/testscripts/ToolsQAAPITest.java
@@ -1,5 +1,7 @@
 package testscripts;
 
+import org.apache.commons.codec.binary.Base64;
+import org.junit.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -7,6 +9,13 @@ import io.restassured.path.json.JsonPath;
 import pojo.ToolsQA;
 
 public class ToolsQAAPITest {
+	
+	
+	@BeforeClass
+	void beforeClass()
+	{
+		RestAssured.useRelaxedHTTPSValidation();
+	}
 	
 	@Test
 	void testToolsQAAPI()
@@ -18,6 +27,22 @@ public class ToolsQAAPITest {
 		
 		
 		System.out.println("Title of second book is: "+respBody.getBooks().get(1).getTitle());
+		
+		
+	}
+	
+	@Test
+	void testToolsQAAPI_BasicAuth()
+	{
+		
+		RestAssured.baseURI="https://bookstore.toolsqa.com";
+		String cred="TOOLSQA-Test:Test@@123";
+		String stringCred=Base64.encodeBase64(cred.getBytes()).toString();
+		ToolsQA respBody=RestAssured.given().log().all().header("Authorization","Basic "+stringCred)
+		.when().get("/BookStore/v1/Books").then().log().all().statusCode(200).extract().response().as(ToolsQA.class);
+		
+		
+		System.out.println("Title of third book is: "+respBody.getBooks().get(2).getTitle());
 		
 		
 	}
