41c617008c3e1723d65197174285dfadd2a0e304
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-12-31 20:03:27 +0530

    Added RS get library api test

---
+++ src/main/java/pojo/RSBooks.java
@@ -0,0 +1,30 @@
+package pojo;
+
+public class RSBooks {
+	
+	private String book_name;
+	private String isbn;
+	private String aisle;
+	
+	public String getBook_name() {
+		return book_name;
+	}
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getAisle() {
+		return aisle;
+	}
+	public void setAisle(String aisle) {
+		this.aisle = aisle;
+	}
+
+	
+
+}
---
+++ src/test/java/testscripts/RS_GetBooksAPI.java
@@ -0,0 +1,31 @@
+package testscripts;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import pojo.RSBooks;
+
+public class RS_GetBooksAPI {
+	
+	
+	
+	@Test
+	void testGetBooksAPI()
+	{
+		
+		
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		RSBooks[] rsBooks=RestAssured.given().log().all().queryParam("AuthorName", "shetty")
+		.when().get("/Library/GetBook.php")
+		.then().assertThat().statusCode(200).extract().response().as(RSBooks[].class);
+		
+		System.out.println(rsBooks[5].getBook_name());
+		
+		System.out.println(rsBooks[5].getIsbn());
+		
+		System.out.println(rsBooks[5].getAisle());
+		
+		
+	}
+
+}
