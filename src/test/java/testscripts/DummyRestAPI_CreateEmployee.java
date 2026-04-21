d83abb51011439f9210cd9f75e63a15c5e2b0d91
Deo Prasad Shaw <deoshaw3@gmail.com>
2026-04-21 10:16:27 +0530

    Added Create Employee API test

--- src/test/java/testscripts/DummyRestAPI_CreateEmployee.java
+++ src/test/java/testscripts/DummyRestAPI_CreateEmployee.java
@@ -13,26 +13,30 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import junit.framework.Assert;
 
 public class DummyRestAPI_CreateEmployee 
 {
 	@Test
 	void validateCreateEmployeeAPITest() throws IOException
 	{
-		RequestSpecification reqSpec=new RequestSpecBuilder().setBaseUri("https://dummy.restapiexample.com")
+		 RequestSpecification reqSpec=new RequestSpecBuilder().setBaseUri("https://dummy.restapiexample.com")
 		.addHeader("Content-Type", "application/json")
-		.setBody(new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+File.separator+"Employee.json")))).build();
-		
-		ResponseSpecification resSpec=new ResponseSpecBuilder().expectStatusCode(200)
-				.expectBody("message",Matchers.equalTo("Successfully! Record has been added."))
-				.build();
-		
-		RestAssured.given().filters(new RequestLoggingFilter(), new ResponseLoggingFilter()).spec(reqSpec)
-		.when().post("/api/v1/create")
-		.then().assertThat().spec(resSpec);
+		.setBody(new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+File.separator+"Employee.json"))))
+		.build();
+		 
+		 ResponseSpecification resSpec=new ResponseSpecBuilder().expectStatusCode(200)
+				 .expectBody("data.name",Matchers.equalTo("test"))
+				 .build();
+		 
+		 JsonPath js=RestAssured.given().filters(new RequestLoggingFilter(),new ResponseLoggingFilter()).spec(reqSpec)
+		 .when().post("/api/v1/create")
+		 .then().assertThat().spec(resSpec).extract().response().jsonPath();
+		 
+		 Assert.assertEquals(js.getString("message"), "Successfully! Record has been added.");
 		
 	}
-
 }
