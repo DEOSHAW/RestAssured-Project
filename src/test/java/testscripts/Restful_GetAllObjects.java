8e50f98d7ea6867b8fd500209574412c6e76e3df
Deo Prasad Shaw <deoshaw3@gmail.com>
2026-05-05 09:23:53 +0530

    Added Get objects api test

--- src/test/java/testscripts/Restful_GetAllObjects.java
+++ src/test/java/testscripts/Restful_GetAllObjects.java
@@ -1,6 +1,6 @@
 package testscripts;
 
-import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -11,24 +11,21 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import junit.framework.Assert;
 
 public class Restful_GetAllObjects 
 {
 	@Test
 	void testGetAllObjectsAPI()
 	{
-		RequestSpecification reqSpec=new RequestSpecBuilder().setBaseUri("https://api.restful-api.dev/").build();
-		ResponseSpecification resSpec=new ResponseSpecBuilder().expectStatusCode(200)
-				.expectBody("name",Matchers.hasItem("Google Pixel 6 Pro"))
-				.build();
+		RequestSpecification reqSpec=new RequestSpecBuilder().setBaseUri("https://api.restful-api.dev").build();
+		
+		ResponseSpecification resSpec=new ResponseSpecBuilder().expectStatusCode(200).build();
 		
 		JsonPath js=RestAssured.given().filters(new RequestLoggingFilter(),new ResponseLoggingFilter()).spec(reqSpec)
 		.when().get("/objects")
 		.then().assertThat().spec(resSpec).extract().response().jsonPath();
+		
 		Assert.assertEquals(js.getList("id").size(), 13);
-		
-		
+		Assert.assertEquals(js.getString("id[3]"), "4");
 	}
-
 }
