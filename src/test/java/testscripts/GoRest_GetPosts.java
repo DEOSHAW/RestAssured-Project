3e411489ed9be1f55e5cdb9f9611df5e8b19c104
Deo Prasad Shaw <deoshaw3@gmail.com>
2026-07-06 11:03:39 +0530

    Added Get Posts API test

--- src/test/java/testscripts/GoRest_GetPosts.java
+++ src/test/java/testscripts/GoRest_GetPosts.java
@@ -1,19 +1,30 @@
 package testscripts;
 
-import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
 
 public class GoRest_GetPosts
 {
 	@Test
 	void testGetPostsAPI()
 	{
-		RestAssured.baseURI="https://gorest.co.in/";
-		RestAssured.given().log().all()
+		RequestSpecification reqSpec=new RequestSpecBuilder().setBaseUri("https://gorest.co.in/").build();
+		ResponseSpecification resSpec=new ResponseSpecBuilder().expectStatusCode(200)
+				.expectBody(JsonSchemaValidator.matchesJsonSchemaInClasspath("Posts.json"))
+				.build();
+		
+		RestAssured.given().filters(new RequestLoggingFilter(), new ResponseLoggingFilter()).spec(reqSpec)
 		.when().get("/public/v2/posts")
-		.then().log().all().statusCode(200).body("title", Matchers.hasItem("Quod cunctatio thema cursim tui."));
+		.then().assertThat().spec(resSpec);
 		
 	}
 
